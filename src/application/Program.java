dab571df4b2eaa9b03c377b17551d7d4fae7fd14
Israel Andrade <israelg.andrade@bol.com.br>
2021-03-24 15:11:39 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -34,6 +35,9 @@ public class Program {
 
         Department obj = new Department(1,"Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new java.util.Date(), 3000.0, obj);
+        System.out.println(seller);
 
     }
 

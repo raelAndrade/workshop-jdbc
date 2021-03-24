d7ffe813061d1af5c9368863333924b30f81c6ab
Israel Andrade <israelg.andrade@bol.com.br>
2021-03-24 12:24:26 -0300

    Recuperar dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,37 @@
 package application;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Connection conn = DB.getConnection();
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = DB.getConnection();
 
-        DB.closeConnection();
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("id") + " - " + rs.getString("name"));
+            }
+
+        } catch (SQLException e) {
+            throw  new DbException(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.getConnection();
+        }
     }
 }

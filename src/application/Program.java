50288ce9ac5f8d841c490b959bbbe1c634cffb51
Israel Andrade <israelg.andrade@bol.com.br>
2021-03-24 14:13:04 -0300

    updated main

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,11 +10,20 @@ import java.text.SimpleDateFormat;
 public class Program {
 
     public static void main(String[] args) {
-
         Connection conn = DB.getConnection();
 
         // Recuperar dados
-        /*
+        recuperarDados(conn);
+
+        // Inserir único dados
+        inserirUnicoDados(conn);
+
+        // Inserir vários dados
+        inserirVariosDados(conn);
+
+    }
+
+    public static void recuperarDados(Connection conn){
         Statement st = null;
         ResultSet rs = null;
         try {
@@ -31,16 +40,13 @@ public class Program {
             DB.closeStatement(st);
             DB.getConnection();
         }
-        */
+    }
 
-        // Inserir dados
+    public static void inserirUnicoDados(Connection conn){
         PreparedStatement st = null;
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         try {
             conn = DB.getConnection();
-
-            // Inserção de dados com um único valor
-            /*
             st = conn.prepareStatement(
                     "insert into seller " +
                             "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
@@ -51,21 +57,30 @@ public class Program {
             st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
             st.setDouble(4, 3000.0);
             st.setInt(5, 4);
-            */
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
-            // Inserção de dados com dois valores ao mesmo tempo
+    public static void inserirVariosDados(Connection conn) {
+        PreparedStatement st = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            conn = DB.getConnection();
             st = conn.prepareStatement("insert into department (Name) values ('D1'),('D2')",
                     Statement.RETURN_GENERATED_KEYS);
 
             int rowsAffected = st.executeUpdate();
 
-            if(rowsAffected > 0) {
+            if (rowsAffected > 0) {
                 ResultSet rs = st.getGeneratedKeys();
-                while(rs.next()){
+                while (rs.next()) {
                     int id = rs.getInt(1);
                     System.out.println("Done! ID = " + id);
                 }
-            }else{
+            } else {
                 System.out.println("No rows affected");
             }
         } catch (SQLException e) {
